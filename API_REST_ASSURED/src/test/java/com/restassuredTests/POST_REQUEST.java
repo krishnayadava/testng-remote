327d51266c94e5d0bb15ec989a52ef2895961d93
Abhinav Ranajn <abhinav1991ranjan@gmail.com>
2019-06-23 12:43:03 +0530

    API_REST_ASSURED

---
+++ API_REST_ASSURED/src/test/java/com/restassuredTests/POST_REQUEST.java
@@ -0,0 +1,13 @@
+package com.restassuredTests;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class POST_REQUEST {
+	
+	public void registerName() {
+		
+		given()
+		
+		      
+	}
+
+}
---
+++ API_REST_ASSURED/src/test/java/com/restassuredTests/RestUtils.java
@@ -0,0 +1,33 @@
+package com.restassuredTests;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class RestUtils {
+	
+	
+	public static String getFirstName() {
+		 String generatedString=RandomStringUtils.randomAlphabetic(1);
+		 return generatedString;
+	}
+	
+	public static String getLastName() {
+		 String generatedString=RandomStringUtils.randomAlphabetic(1);
+		 return generatedString;
+	}
+	
+	public static String getUserName() {
+		 String generatedString=RandomStringUtils.randomAlphabetic(1);
+		 return generatedString;
+	}
+	
+	public static String emailId() {
+		 String generatedString=RandomStringUtils.randomAlphabetic(1);
+		 return generatedString;
+	}
+	
+	public static String getPassword() {
+		 String generatedString=RandomStringUtils.randomAlphabetic(1);
+		 return generatedString;
+	}
+
+}
